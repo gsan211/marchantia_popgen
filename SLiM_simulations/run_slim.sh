b6cbc78c4307aca78737c8f0b8789eac7fb41e82
gsan211 <36670832+gsan211@users.noreply.github.com>
2023-07-05 17:24:03 -0400

    Update run_slim.sh

--- SLiM_simulations/run_slim.sh
+++ SLiM_simulations/run_slim.sh
@@ -1,14 +1,14 @@
 #run SLiM simulations 
 
 cd "/plas1/george.sandler/marchantia_popgen_newgenome/slim/fac_sex/"
-parallel --jobs 20 /plas1/apps/SLiM.2.4.2/bin/slim -d o={} "/plas1/george.sandler/marchantia_popgen_newgenome/slim/slim_admix_facsex.eidos" ::: 9 10 11 12 13 14 15
+parallel --jobs 20 /plas1/apps/SLiM.2.4.2/bin/slim -d o={} "/plas1/george.sandler/marchantia_popgen_newgenome/slim/slim_admix_facsex.eidos" ::: 1 2 3 4 5
 
 
 cd "/plas1/george.sandler/marchantia_popgen_newgenome/slim/fac_sex2k/"
-parallel --jobs 20 /plas1/apps/SLiM.2.4.2/bin/slim -d o={} "/plas1/george.sandler/marchantia_popgen_newgenome/slim/slim_admix_facsex2k.eidos" ::: 2
+parallel --jobs 20 /plas1/apps/SLiM.2.4.2/bin/slim -d o={} "/plas1/george.sandler/marchantia_popgen_newgenome/slim/slim_admix_facsex2k.eidos" ::: 1 2 3 4 5
 
 
 
 cd "/plas1/george.sandler/marchantia_popgen_newgenome/slim/low_rho/"
-parallel --jobs 20 /plas1/apps/SLiM.2.4.2/bin/slim -d o={} "/plas1/george.sandler/marchantia_popgen_newgenome/slim/slim_admix_lowrho.eidos" ::: 1 2 3 4 5 6 7 8
+parallel --jobs 20 /plas1/apps/SLiM.2.4.2/bin/slim -d o={} "/plas1/george.sandler/marchantia_popgen_newgenome/slim/slim_admix_lowrho.eidos" ::: 1 2 3 4 5
 
